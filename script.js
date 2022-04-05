d9495db03e627e0f3736dcc955e6377d8c099cb4
Shaq Deff <shaqdeff@MacBook-Pro.local>
2022-04-05 20:39:42 +0300

    Modified styles.css file

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const navLinks = document.getElementById('navLinks');
 
-const showMenu = function showMenu() {
+  function showMenu() {
   navLinks.style.right = '0';
 }
-const hideMenu = function hideMenu() {
-  navLinks.style.right = '-42rem';
+  function hideMenu() {
+  navLinks.style.right = '-400px';
 }
