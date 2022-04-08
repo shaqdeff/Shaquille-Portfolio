5a044d25d97638b65a38a3cd793d6ac0685a84cf
Shaq Deff <shaqdeff@MacBook-Pro.local>
2022-04-08 19:02:49 +0300

    Rectify email vaildation

--- javascript/form.js
+++ javascript/form.js
@@ -3,10 +3,10 @@ const email = document.getElementById('email');
 const errorMessage = document.getElementById('err-msg');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    if (email.value === email.value.toLowerCase()) {
-                  form.submit();
-    } else {
-                  errorMessage.innerText = 'Please type your e-mail in lowercase';
-    }
+              event.preventDefault();
+              if (email.value === email.value.toLowerCase()) {
+                            form.submit();
+              } else {
+                            errorMessage.innerText = 'Please type your e-mail in lowercase';
+              }
 });
